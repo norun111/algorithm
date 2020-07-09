c6e9f47aea72a8b225ebf1709444ea8db3e1609a
tomoyaueno29 <u.t012807@gmail.com>
2020-07-09 18:07:19 +0900

    Create 野球の審判.go

---
+++ 過去問/野球の審判.go
@@ -0,0 +1,38 @@
+package main
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+)
+func main(){
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	loopNum, _ := strconv.Atoi(sc.Text())
+
+	var strikeNum int = 0
+	var ballNum int = 0
+
+	for i:=0; i<loopNum; i++ {
+		sc.Scan()
+		judge := sc.Text()
+
+		if judge == "ball" {
+			if ballNum+1 == 4 {
+				fmt.Println("fourball!")
+				break
+			}
+
+			fmt.Println("ball!")
+			ballNum++
+		} else if judge == "strike" {
+
+			if strikeNum+1 == 3 {
+				fmt.Println("out!")
+				break
+			}
+			fmt.Println("strike!")
+			strikeNum++
+		}
+	}
+}
