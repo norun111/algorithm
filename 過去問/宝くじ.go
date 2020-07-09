0fab997c35297f4e097fba115fc766d5953e4ad7
tomoyaueno29 <u.t012807@gmail.com>
2020-07-09 18:07:14 +0900

    Create 宝くじ.go

---
+++ 過去問/宝くじ.go
@@ -0,0 +1,34 @@
+package main
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+)
+func main(){
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	correct, _ := strconv.Atoi(sc.Text())
+	correctStr := sc.Text()
+
+	sc.Scan()
+	loopNum, _ := strconv.Atoi(sc.Text())
+
+	for i:=0; i<loopNum; i++ {
+		sc.Scan()
+		challenge, _ := strconv.Atoi(sc.Text())
+		challengeStr := sc.Text()
+		// fmt.Println(charengeStr[len(charengeStr)-3:])
+		if challenge == correct {
+			fmt.Println("first")
+		} else if challenge == correct+1 || challenge == correct-1 {
+			fmt.Println("adjacent")
+		} else if challengeStr[len(challengeStr)-4:] == correctStr[len(correctStr)-4:] {
+			fmt.Println("second")
+		} else if challengeStr[len(challengeStr)-3:] == correctStr[len(correctStr)-3:] {
+			fmt.Println("third")
+		} else {
+			fmt.Println("blank")
+		}
+	}
+}
