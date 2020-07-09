366d16acd2a1638c7e3920f7e6cd07aaa06aa58e
tomoyaueno29 <u.t012807@gmail.com>
2020-07-09 18:07:10 +0900

    Create 歩数.go

---
+++ 早解/歩数.go
@@ -0,0 +1,26 @@
+package main
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func main(){
+
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	slice := strings.Split(sc.Text(), " ")
+	km, _ := strconv.Atoi(slice[0])
+	km = km*100000
+	hohaba, _ := strconv.Atoi(slice[1])
+
+	num := km/hohaba
+
+	if num >= 10000 {
+		fmt.Println("yes")
+	} else {
+		fmt.Println("no")
+	}
+}
