0ae1274eb4267c2ddffdee282d27dbda809cd9b7
tomoyaueno29 <u.t012807@gmail.com>
2020-07-09 18:07:02 +0900

    Create main.go

---
+++ sample/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func main() {
+	//切り上げ
+	i := 0
+	v := float64(1.234)
+	i = int(math.Ceil(v))
+	fmt.Println(i)
+
+	//切り捨て
+	i = 0
+	v = float64(1.234)
+	i = int(math.Floor(v))
+	fmt.Println(i)
+}
