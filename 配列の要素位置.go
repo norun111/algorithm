044bb8747f734db04d2285f837a9d4a7a8491184
tomoyaueno29 <u.t012807@gmail.com>
2020-07-10 16:06:51 +0900

    Update 配列の要素位置.go

--- 配列の要素位置.go
+++ 配列の要素位置.go
@@ -13,3 +13,18 @@ func index(slice []string, str string) int {
 
 	return index // indexが0は配列に文字列が存在しない場合
 }
+
+//単語が配列にあるかどうかの判断関数
+func isContain(slice []string, str string) bool {
+
+	condition := false
+	for _, s := range slice {
+
+		if s == str {
+			condition = true
+			break //一番初めだけ検索する
+		}
+	}
+
+	return condition //なければfalseを返す
+}
