75d3dbfd9c449296ad9fb1d886f04d834acba761
tomoyaueno29 <u.t012807@gmail.com>
2020-07-12 00:22:10 +0900

    Create C081.go

---
+++ C/C081.go
@@ -0,0 +1,68 @@
+package main
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+	"strconv"
+)
+func main(){
+	// 自分の得意な言語で
+	// Let's チャレンジ！！
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	loopNum, _ := strconv.Atoi(sc.Text())
+
+	//オブジェクトの作成
+	obj := make(map[string]map[string]int, 0)
+
+	typeList := []string{}
+	RLlist := []string{}
+
+	for i:=0; i<loopNum; i++ {
+		sc.Scan()
+		s := strings.Split(sc.Text(), " ")
+		obj[s[0]] = map[string]int{
+			"R":0,
+			"L":0,
+		}
+		typeList = append(typeList, s[0])
+		RLlist = append(RLlist, s[1])
+	}
+	// fmt.Println(obj)
+	// fmt.Println(typeList)
+	// fmt.Println(RLlist)
+
+	for key := range obj {
+
+		for i:=0; i<loopNum; i++ {
+
+			if key==typeList[i] {
+
+				if RLlist[i] == "R"{
+
+					obj[key]["R"] += 1
+				} else if RLlist[i] == "L" {
+					obj[key]["L"] += 1
+				}
+			}
+		}
+	}
+	// fmt.Println(obj)
+
+	sum := 0
+
+	for key := range obj {
+
+		add := 0
+		add+=obj[key]["R"]
+		add+=obj[key]["L"]
+
+		// fmt.Println(add)
+		result := add/2
+		sum+=result
+	}
+
+	fmt.Println(sum)
+
+}
