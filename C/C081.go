07f6f8157c2769e02ff1ca40a4d26511d1e188e5
tomoyaueno29 <u.t012807@gmail.com>
2020-07-12 17:59:50 +0900

    Update C081.go

--- C/C081.go
+++ C/C081.go
@@ -1,12 +1,14 @@
 package main
+
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
-	"strings"
 	"strconv"
+	"strings"
 )
-func main(){
+
+func main() {
 	// 自分の得意な言語で
 	// Let's チャレンジ！！
 	sc := bufio.NewScanner(os.Stdin)
@@ -19,12 +21,12 @@ func main(){
 	typeList := []string{}
 	RLlist := []string{}
 
-	for i:=0; i<loopNum; i++ {
+	for i := 0; i < loopNum; i++ {
 		sc.Scan()
 		s := strings.Split(sc.Text(), " ")
 		obj[s[0]] = map[string]int{
-			"R":0,
-			"L":0,
+			"R": 0,
+			"L": 0,
 		}
 		typeList = append(typeList, s[0])
 		RLlist = append(RLlist, s[1])
@@ -35,11 +37,11 @@ func main(){
 
 	for key := range obj {
 
-		for i:=0; i<loopNum; i++ {
+		for i := 0; i < loopNum; i++ {
 
-			if key==typeList[i] {
+			if key == typeList[i] {
 
-				if RLlist[i] == "R"{
+				if RLlist[i] == "R" {
 
 					obj[key]["R"] += 1
 				} else if RLlist[i] == "L" {
@@ -55,12 +57,12 @@ func main(){
 	for key := range obj {
 
 		add := 0
-		add+=obj[key]["R"]
-		add+=obj[key]["L"]
+		add += obj[key]["R"]
+		add += obj[key]["L"]
 
 		// fmt.Println(add)
-		result := add/2
-		sum+=result
+		result := add / 2
+		sum += result
 	}
 
 	fmt.Println(sum)
