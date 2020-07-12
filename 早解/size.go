0804deb44845c70afc6ccdcc4d6eaf09eb0cdd93
tomoyaueno29 <u.t012807@gmail.com>
2020-07-12 17:59:57 +0900

    Update size.go

--- 早解/size.go
+++ 早解/size.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"bufio"
 	"fmt"
@@ -6,13 +7,13 @@ import (
 	"strconv"
 )
 
-func main(){
+func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
 	num, _ := strconv.ParseFloat(sc.Text(), 32)
 
-	us := num-18.0
-	uk := num-18.5
+	us := num - 18.0
+	uk := num - 18.5
 	//小数点第一位まで取得　prec
-	fmt.Println(strconv.FormatFloat(us, 'f', 1, 32)+" "+strconv.FormatFloat(uk, 'f', 1, 32))
+	fmt.Println(strconv.FormatFloat(us, 'f', 1, 32) + " " + strconv.FormatFloat(uk, 'f', 1, 32))
 }
