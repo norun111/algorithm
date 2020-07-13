4688df4aae7340e7d3804c205ba0f4babd41d9df
tomoyaueno29 <u.t012807@gmail.com>
2020-07-13 13:23:46 +0900

    Create B051.go

---
+++ B/B051.go
@@ -0,0 +1,75 @@
+//全て通っていない
+
+package main
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+	"fmt"
+	"sort"
+)
+func main(){
+
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	loopNum, _ := strconv.Atoi(sc.Text())
+
+	tateList := make([][]int, loopNum)
+
+	numCorrect := []int{}
+
+	for i:=0; i<loopNum; i++ {
+		sum := 0
+		sc.Scan()
+		s := strings.Split(sc.Text(), " ")
+
+		for j:=0; j<loopNum; j++ {
+
+			n, _ := strconv.Atoi(s[j])
+			tateList[j] = append(tateList[j], n)
+			sum+=n
+		}
+		numCorrect = append(numCorrect, sum)
+	}
+	sort.Ints(numCorrect)
+	correct := numCorrect[len(numCorrect)-1]
+
+	for _, t := range tateList {
+
+		tateSum := 0
+		zeroIndex := 0
+
+		for i:=0; i<loopNum; i++ {
+
+			tateSum+=t[i]
+			if t[i] == 0 {
+
+				zeroIndex = i
+			}
+		}
+
+		if tateSum < correct {
+			diff := correct - tateSum
+			t[zeroIndex] = diff
+		}
+	}
+	resultList := make([][]string, loopNum)
+
+	// 	fmt.Println(tateList)
+
+	for _, t := range tateList {
+
+
+		for j:=0; j<loopNum; j++ {
+			str := strconv.Itoa(t[j])
+			resultList[j] = append(resultList[j], str)
+		}
+	}
+	// 	fmt.Println(resultList)
+
+	for _, r := range resultList {
+		fmt.Println(strings.Join(r, " "))
+	}
+
+}
