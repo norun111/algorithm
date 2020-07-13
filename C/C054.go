3fbbd85812c9d3df069094d6c26c9512ff57af2e
tomoyaueno29 <u.t012807@gmail.com>
2020-07-13 13:23:49 +0900

    Create C054.go

---
+++ C/C054.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+	"fmt"
+)
+func main(){
+
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	s := strings.Split(sc.Text(), " ")
+	loopNum, _ := strconv.Atoi(s[0])
+	speedLimit, _ := strconv.Atoi(s[1])
+
+	obj := make(map[int]int, loopNum)
+
+	for i:=0; i<loopNum; i++ {
+		sc.Scan()
+		sl := strings.Split(sc.Text(), " ")
+
+		dist, _ := strconv.Atoi(sl[0])
+		speed, _ := strconv.Atoi(sl[1])
+
+		obj[dist] = speed
+	}
+
+	// 	fmt.Println(obj)
+
+	resultList := []int{}
+
+	for key1 := range obj {
+		for key2 := range obj {
+
+			if key1 < key2 {
+				dif := key2 - key1
+				speedDif := obj[key2] - obj[key1]
+
+				r := speedDif/dif
+				resultList = append(resultList, r)
+			}
+		}
+	}
+	sin := false
+
+	for _, re := range resultList {
+		if speedLimit < re {
+			fmt.Println("YES")
+			sin = true
+			break
+		}
+	}
+
+	if sin == false {
+		fmt.Println("NO")
+	}
+}
