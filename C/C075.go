ffe15e90f0b83d9be9c2b5094dc4f08d12d50b96
tomoyaueno29 <u.t012807@gmail.com>
2020-08-03 21:54:45 +0900

    Create C075.go

---
+++ C/C075.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main(){
+
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	l := strings.Split(sc.Text(), " ")
+	charge, _ := strconv.Atoi(l[0])
+	num, _ := strconv.Atoi(l[1])
+
+	point := 0
+
+	for i:=0; i<num; i++ {
+		sc.Scan()
+		fee, _ := strconv.Atoi(sc.Text())
+
+		if point >= fee {
+			point -= fee
+			fmt.Println(charge, point)
+		} else if charge >= fee {
+			charge -= fee
+			p := fee/10
+			point+=p
+			fmt.Println(charge, point)
+		}
+	}
+}
