f86ab6b38a1546e59b8559171814a15fe693e2a0
tomoyaueno29 <u.t012807@gmail.com>
2020-08-04 21:39:58 +0900

    Create B055.go

---
+++ B/B055.go
@@ -0,0 +1,64 @@
+package main
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"math"
+	"sort"
+)
+func main(){
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	list := strings.Split(sc.Text(), " ")
+
+	taxiNum, _ := strconv.Atoi(list[0])
+	distance, _ := strconv.Atoi(list[1])
+
+	feeList := []int{}
+
+	for i:=0; i<taxiNum; i++ {
+		sc.Scan()
+		li := strings.Split(sc.Text(), " ")
+		firstDistance, _ := strconv.Atoi(li[0])
+		firstFee, _ := strconv.Atoi(li[1])
+		addDistance, _ := strconv.Atoi(li[2])
+		addFee, _ := strconv.Atoi(li[3])
+
+		sum := 0
+
+		if distance >= firstDistance {
+			sum+=firstFee
+			remainingDistance := distance - firstDistance
+
+			if remainingDistance <= addDistance {
+				sum+=addFee
+			} else if remainingDistance % addDistance == 0 {
+				div := (float64(remainingDistance) / float64(addDistance))
+				in := int(math.Ceil(div))
+				sum+=(in*addFee)
+				sum+=addFee
+			} else {
+				div := (float64(remainingDistance) / float64(addDistance))
+				in := int(math.Ceil(div))
+				sum+=(in*addFee)
+			}
+
+
+		} else if distance < firstDistance {
+			sum+=firstFee
+		}
+		feeList = append(feeList, sum)
+	}
+
+	sort.Ints(feeList)
+
+	min := strconv.Itoa(feeList[0])
+	max := strconv.Itoa(feeList[len(feeList)-1])
+
+	l := []string{min, max}
+
+	fmt.Println(strings.Join(l, " "))
+
+}
