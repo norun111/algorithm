d617322ebbb6eb042e3ec4f9b40b5bacf2712724
tomoyaueno29 <u.t012807@gmail.com>
2020-08-13 21:45:03 +0900

    Update C062.go

--- C/C062.go
+++ C/C062.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func main(){
+func main() {
 	// Your code here!
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
@@ -16,7 +16,7 @@ func main(){
 	melon := 0
 	sum := 0
 
-	for i:=1; i<num+1; i++ {
+	for i := 1; i < num+1; i++ {
 		sc.Scan()
 		text := sc.Text()
 
@@ -27,13 +27,12 @@ func main(){
 		}
 	}
 
-
-	for i:=melon+1; i<num+1; i++ {
+	for i := melon + 1; i < num+1; i++ {
 		sc.Scan()
 		text := sc.Text()
 		if text == "melon" {
 
-			if melon + 10 < i {
+			if melon+10 < i {
 				sum++
 				melon = i
 			}
