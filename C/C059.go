faffa9f2db37b9276710a0695482bc86a611ce29
tomoyaueno29 <u.t012807@gmail.com>
2020-08-13 21:45:02 +0900

    Update C059.go

--- C/C059.go
+++ C/C059.go
@@ -2,28 +2,28 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
-	"fmt"
 )
 
-func main(){
+func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
 	loopNum, _ := strconv.Atoi(sc.Text())
 
 	slice := make([]int, 4)
 
-	for i:=0; i<loopNum; i++ {
+	for i := 0; i < loopNum; i++ {
 		sc.Scan()
 		s := strings.Split(sc.Text(), "")
 		// fmt.Println(s)
 		// fmt.Println("*****************")
-		for j:=0; j<4; j++ {
+		for j := 0; j < 4; j++ {
 			// fmt.Println(s[j])
 			if s[j] == "1" {
-				slice[j]+=1
+				slice[j] += 1
 			}
 		}
 	}
