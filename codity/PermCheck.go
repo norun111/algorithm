c0966e86f758599d494f60208622524cb21bf16a
tomoyaueno29 <u.t012807@gmail.com>
2020-09-06 19:50:36 +0900

    Create PermCheck.go

---
+++ codity/PermCheck.go
@@ -0,0 +1,39 @@
+package codity
+
+// 88%
+
+// you can also use imports, for example:
+import (
+	"sort"
+	// "fmt"
+)
+// import "os"
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A []int) int {
+	// write your code in Go 1.4
+	sort.Ints(A)
+
+	result := 1
+
+	if len(A) == 1 {
+		if A[0] == 1 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	for i, _ := range A {
+		if i > 0 {
+			if A[i] != A[i-1]+1 {
+				result = 0
+				break
+			}
+		}
+	}
+
+	return result
+}
