03931d090a461a6311eed2fe96b5f0d3f6162117
tomoyaueno29 <u.t012807@gmail.com>
2020-09-07 21:32:28 +0900

    Update OddOccurrencesInArray.go

--- codity/OddOccurrencesInArray.go
+++ codity/OddOccurrencesInArray.go
@@ -1,16 +1,34 @@
 package Codity
 
+
+// 100%
+import (
+	"sort"
+)
+
+func Solution(A []int) int {
+	// write your code in Go 1.4
+	sort.Ints(A)
+
+	for i:=0; i<len(A)-1; {
+		if A[i] != A[i+1] {
+
+			return A[i]
+		}
+		i = i+2
+	}
+	return A[len(A)-1]
+}
+
+// Timeout
+
 func Solution(A []int) int {
 	// write your code in Go 1.4
 	m := make(map[int]int, 0)
-
-	if len(A) == 1 {
-		return A[0]
+	for _, a := range A {
+		m[a] = 0
 	}
 
-	for i:=0; i<len(A); i++ {
-		m[A[i]] = 0
-	}
 
 	for key, _ := range m {
 		for _, a := range A {
@@ -19,10 +37,10 @@ func Solution(A []int) int {
 			}
 		}
 	}
-	result := 0
 
-	for key, value := range m {
-		if value%2 != 0 {
+	result := 0
+	for key, _ := range m {
+		if m[key]%2!=0 {
 			result = key
 		}
 	}
