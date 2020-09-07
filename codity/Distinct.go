6ba27c329b72cbec62a4f2212cc1eb21a4d725df
tomoyaueno29 <u.t012807@gmail.com>
2020-09-07 20:01:13 +0900

    Create Distinct.go

---
+++ codity/Distinct.go
@@ -0,0 +1,19 @@
+package codity
+
+func removeDuplicate(args []int) int {
+	encountered := map[int]bool{}
+
+	count := 0
+
+	for i:=0; i<len(args); i++ {
+		if !encountered[args[i]] {
+			count++
+			encountered[args[i]] = true
+		}
+	}
+	return count
+}
+
+func Solution(A []int) int {
+	return removeDuplicate(A)
+}
