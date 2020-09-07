aa1e9c405af696e826d56b55259c5de2c109a0b4
tomoyaueno29 <u.t012807@gmail.com>
2020-09-07 20:05:38 +0900

    Create 要素の分割.go

---
+++ normal/要素の分割.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var intSlices, _ = SplitSlice([]int{1, 2, 3, 4, 5}, 2)
+	for _, is := range intSlices {
+		fmt.Printf("int slice %v \n", is.([]int))
+	}
+
+	var strSlices, _ = SplitSlice([]string{"A", "B", "C", "D", "E"}, 2)
+	for _, ss := range strSlices {
+		fmt.Printf("string slice %v \n", ss.([]string))
+	}
+}
+
+func SplitSlice(list interface{}, size int) ([]interface{}, error) {
+	if size <= 0 {
+		return nil, fmt.Errorf("size need positive number")
+	}
+
+	// sliceかどうかのチェック
+	if reflect.TypeOf(list).Kind() != reflect.Slice {
+		return nil, fmt.Errorf("src must be slice")
+	}
+
+	var result = []interface{}{}
+
+	var v = reflect.ValueOf(list)
+	for i := 0; i < v.Len(); i += size {
+		if i+size <= v.Len() {
+			result = append(result, v.Slice(i, i+size).Interface())
+		} else {
+			result = append(result, v.Slice(i, v.Len()).Interface())
+		}
+	}
+
+	return result, nil
+}
