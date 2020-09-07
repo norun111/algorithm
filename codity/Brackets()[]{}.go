d9e0b28fad53e69784191c4f206342f9b53251f5
tomoyaueno29 <u.t012807@gmail.com>
2020-09-07 20:01:11 +0900

    Create Brackets()[]{}.go

---
+++ codity/Brackets()[]{}.go
@@ -0,0 +1,50 @@
+package codity
+
+// 100%
+
+import (
+	"strings"
+	// "fmt"
+)
+
+func isValidPair(left, right string) bool {
+	if left == "(" && right == ")" {
+		return true
+	} else if left == "[" && right == "]"{
+		return true
+	} else if left == "{" && right == "}"{
+		return true
+	}
+	return false
+}
+
+func Solution(S string) int {
+	// write your code in Go 1.4
+	strSlice := strings.Split(S, "")
+
+	stack := make([]string, 0)
+	for _, str := range strSlice {
+		if str == "[" || str == "{" || str == "(" {
+			stack = append(stack, str)
+		} else {
+			if len(stack) == 0 {
+				return 0
+			}
+			// stackに"["か"{"か"("が存在する場合
+			last := stack[len(stack)-1]  // 末尾を取得
+			stack = stack[:len(stack)-1] // 末尾以外を更新
+
+			if !isValidPair(last, str) {
+				return 0
+			}
+
+		}
+	}
+
+	if len(stack) != 0 {
+		return 0
+	} else {
+		return 1
+	}
+
+}
