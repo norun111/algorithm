d0a0eb0ac7b250c50a43e24dfb70348fa6eafe31
tomoyaueno29 <u.t012807@gmail.com>
2020-09-07 20:05:39 +0900

    Create 配列の要素位置.go

---
+++ normal/配列の要素位置.go
@@ -0,0 +1,30 @@
+package main
+
+func index(slice []string, str string) int {
+
+	index := -1
+	for i, s := range slice {
+
+		if s == str {
+			index = i
+			break //一番初めだけ検索する
+		}
+	}
+
+	return index // indexが-1は配列に文字列が存在しない場合
+}
+
+//単語が配列にあるかどうかの判断関数
+func isContain(slice []string, str string) bool {
+
+	condition := false
+	for _, s := range slice {
+
+		if s == str {
+			condition = true
+			break //一番初めだけ検索する
+		}
+	}
+
+	return condition //なければfalseを返す
+}
