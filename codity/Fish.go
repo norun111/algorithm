c396c528021800d8c71c919b7c70c142a4263400
tomoyaueno29 <u.t012807@gmail.com>
2020-09-07 20:01:18 +0900

    Create Fish.go

---
+++ codity/Fish.go
@@ -0,0 +1,28 @@
+package codity
+
+func Solution(A []int, B []int) int {
+
+	stack := make([]int, 0)
+	survivals := 0
+
+	for i, _ := range A {
+		if B[i] == 0 {
+			for len(stack) != 0 {
+				if stack[len(stack)-1] > A[i] {
+					break
+				} else {
+					stack = stack[:len(stack)-1]
+				}
+			}
+			if len(stack) == 0 {
+				survivals += 1
+			}
+
+		} else {
+			stack = append(stack, A[i])
+		}
+	}
+	survivals+=len(stack)
+
+	return survivals
+}
