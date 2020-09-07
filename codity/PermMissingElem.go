758e3e1e7405061a82f33f59ca30440966957f35
tomoyaueno29 <u.t012807@gmail.com>
2020-09-07 22:01:48 +0900

    Update PermMissingElem.go

--- codity/PermMissingElem.go
+++ codity/PermMissingElem.go
@@ -1,33 +1,14 @@
 package Codity
 
-// you can also use imports, for example:
-import (
-	"sort"
-	// "fmt"
-)
-// import "os"
-
-// you can write to stdout for debugging purposes, e.g.
-// fmt.Println("this is a debug message")
-
 func Solution(A []int) int {
-	sort.Ints(A)
+	// write your code in Go 1.4
+	should_be := len(A)
+	sum_is := 0
 
-	nowNum := 0
-
-
-	for i:=0; i<len(A); i++ {
-		if i==0 {
-			nowNum = A[i]
-		} else {
-			if A[i] == nowNum+1 {
-				nowNum = A[i]
-			} else {
-				nowNum = nowNum + 1
-				break
-			}
-		}
+	for idx, _ := range A {
+		sum_is += A[idx]
+		should_be += idx+1
 	}
 
-	return nowNum
+	return should_be - sum_is +1
 }
