7fbcf0f646aa512ce020ec4fe6c1ff2f1d9955b7
tomoyaueno29 <u.t012807@gmail.com>
2020-09-07 20:01:20 +0900

    Create MaxProductOfThree.go

---
+++ codity/MaxProductOfThree.go
@@ -0,0 +1,32 @@
+package codity
+// 100%
+
+import (
+	"sort"
+)
+
+func Solution(A []int) int {
+	sort.Ints(A)
+
+	maxThree := A[len(A)-3:]
+
+	minTwo := A[0:2]
+
+	sum1 := 1
+	for _, max := range maxThree {
+		sum1*=max
+	}
+
+	sum2 := minTwo[0]*minTwo[1]*maxThree[len(maxThree)-1]
+	result := 0
+
+	if sum1 > sum2 {
+		result = sum1
+	} else if sum1 < sum2 {
+		result = sum2
+	} else if sum1 == sum2 {
+		result = sum1
+	}
+
+	return result
+}
